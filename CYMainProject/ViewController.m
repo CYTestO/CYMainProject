bbc2d4e131fbae79ff4a67c37c5302bf7b33f7a5
sobeycloud <caiyuan@chinamcloud.com>
2017-03-23 10:17:27 +0800

    first commit

---
+++ CYMainProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CYMainProject
+//
+//  Created by sobeycloud on 2017/3/23.
+//  Copyright © 2017年 sobeycloud. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
